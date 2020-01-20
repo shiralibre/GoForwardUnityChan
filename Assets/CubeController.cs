389b454a8c5f7a8fee64e7a2a886878e983ac5ad
yuutashiraishi <yuta.winner1@gmail.com>
2020-01-20 16:22:20 +0900

    Lesson7課題
    効果音をつける

---
+++ Assets/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        this.audioSource = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag!="Player" )
+        {
+            audioSource.Play();
+        }
+    }
+}
